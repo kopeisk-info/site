2e11de3f663887a8530e263f0a13b2b92512fc71
Anton Popov <a.popov@kit.team>
2020-08-27 14:06:44 +0500

    Текущая версия компонента

--- app/View/Components/VkPost.php
+++ app/View/Components/VkPost.php
@@ -3,15 +3,17 @@
 namespace App\View\Components;
 
 use Illuminate\View\Component;
+use App\VkPost as Post;
 
 class VkPost extends Component
 {
     public $id, $name, $screen_name, $photo,
-        $post_id, $post_type, $date, $text,
-        $from_link, $post_link,
-        $action;
+        $post_id, $post_type,
+        $date, $text, $image,
+        $action, $from_link, $post_link,
+        $comments, $likes, $reposts, $views;
 
-    private $copy_history;
+    public $repost;
 
     /**
      * Create a new component instance.
@@ -41,7 +43,7 @@ class VkPost extends Component
         $this->screen_name = $post->from->screen_name;
         $this->photo = $post->from->photo_50;
 
-        $this->post_id = $post->from->id;
+        $this->post_id = $post->id;
         $this->post_type = $post->post_type;
         $this->date = $post->date;
         $this->text = $post->text;
@@ -49,13 +51,48 @@ class VkPost extends Component
         $this->from_link = "https://vk.com/{$this->screen_name}";
         $this->post_link = "{$this->from_link}?w=wall-{$this->id }_{$this->post_id}";
 
-        $this->action = 'разместил пост';
-        if ('post' == $this->post_type) {
+        if ($post->attachments) {
+            $attachment = current($post->attachments);
+            if ('photo' === $attachment['type']) {
+                if (!isset($attachment['photo']['sizes']['7']['url'])) {
+                    dd($attachment['photo']['sizes']);
+                    $this->image = $attachment['photo']['sizes']['3']['url'];
+                } else {
+                    $this->image = $attachment['photo']['sizes']['7']['url'];
+                }
+            } elseif ('video' === $attachment['type']) {
+                if (!isset($attachment['video']['image']['7']['url'])) {
+                    $this->image = $attachment['video']['image']['3']['url'];
+                } else {
+                    $this->image = $attachment['video']['image']['7']['url'];
+                }
+            } elseif (('link' === $attachment['type']) && isset($attachment['link']['photo'])) {
+                $this->action = 'ссылка';
+                if (!isset($attachment['link']['photo']['sizes']['7'])) {
+                    $this->image = $attachment['link']['photo']['sizes']['3']['url'];
+                } else {
+                    $this->image = $attachment['link']['photo']['sizes']['7']['url'];
+                }
+            }
+        }
 
+        if ($post->comments && $post->comments['can_post']) {
+            $this->comments = $post->comments['count'];
+        }
+        if ($post->likes && $post->likes['can_like']) {
+            $this->likes = $post->likes['count'];
+        }
+        if ($this->reposts) {
+            $this->reposts = $post->reposts['count'];
+        }
+        if ($this->views) {
+            $this->views = $post->views['count'];
         }
 
         if ($post->copy_history) {
-            $this->copy_history = $post->copy_history;
+            $this->action = 'репост';
+            $post = Post::make($post->copy_history->get(0));
+            $this->repost = new self($post);
         }
     }
 }
