58ec065b0a18211605b777411a81694cc3b0df6f
Anton Popov <a.popov@kit.team>
2020-09-02 16:23:29 +0500

    Рабочий набросок

--- app/Console/Commands/Vk/GetPosts.php
+++ app/Console/Commands/Vk/GetPosts.php
@@ -6,16 +6,22 @@ use http\Exception\RuntimeException;
 use Illuminate\Support\Facades\Log;
 
 use VK\Client\VKApiClient;
+use App\VkUser;
+use App\VkGroup;
 use App\VkPost;
 
 class GetPosts extends Command
 {
+    private $profiles = [];
+    private $groups = [];
+    private $posts = [];
+
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'vk:get-posts {id?} {--group}';
+    protected $signature = 'vk:get-posts {id} {--group}';
 
     /**
      * The console command description.
@@ -41,33 +47,69 @@ class GetPosts extends Command
      */
     public function handle()
     {
-        if ($id = $this->argument('id')) {
-            $vk = new VKApiClient();
-            try {
-                $response = $vk->wall()->get(env('VK_SERVICE_KEY'), [
-                    'lang' => 'ru',
-                    'owner_id'  => $this->option('group') ? "-$id" : $id,
-                    'filter' => 'all',
-                ]);
+        $id = $this->argument('id');
 
-                foreach($response['items'] as $item) {
-                    VkPost::updateOrCreate([
-                        'owner_id' => $item['owner_id'],
-                        'id' => $item['id']
-                    ], $item);
-                };
+        // Получаем записи со служебной информацией
+        $vk = new VKApiClient();
+        $response = $vk->wall()->get(env('VK_SERVICE_KEY'), [
+            'lang' => 'ru',
+            'owner_id'  => $this->option('group') ? '-'. $id : $id,
+            'extended' => 1,
+            'filter' => 'all',
+            'count' => 10
+        ]);
 
-                $keys = array_reverse(array_column($response['items'], 'id'));
-                $posts = VkPost::where('owner_id', $item['owner_id'])
-                    ->whereNotIn('id', $keys)
-                    ->whereBetween('id', [current($keys), end($keys)])
-                    ->get();
-                foreach ($posts as $post) {
-                    $post->delete();
-                }
-            } catch (RuntimeException $exception) {
-                Log::info('Ошибка загрузки id '. $id);
+        $this->posts = $response['items'];
+        $this->profiles = $response['profiles'];
+        $this->groups = $response['groups'];
+
+        // Добавление отсутсвующих зависимостей(профили)
+        $profileIds = array_column($this->profiles, 'id');
+        $profiles = VkUser::whereIn('id', $profileIds)->get();
+        $ids = array_diff($profileIds , $profiles->modelKeys());
+        foreach ($ids as $key => $id) {
+            $data = $this->profiles[$key];
+            $data['from_copy'] = 1;
+            VkUser::updateOrCreate(['id' => $id], $data);
+        }
+
+        // Добавление отсутсвующих зависимостей(группы)
+        $groupIds = array_column($this->groups, 'id');
+        $groups = VkGroup::whereIn('id', $groupIds)->get();
+        $ids = array_diff($groupIds , $groups->modelKeys());
+        foreach ($ids as $key => $id) {
+            $data = $this->groups[$key];
+            $data['from_copy'] = 1;
+            VkGroup::updateOrCreate(['id' => $id], $data);
+        }
+        dd();
+
+        if ($this->items = $response['items']) {
+            $ids = array_merge(
+                array_column($response['items'], 'from_id'),
+                array_column($response['items'], 'owner_id')
+            );
+            $ids = array_unique($ids);
+
+            foreach($ids as $id) {
+                $id < 0 ? array_push($this->groups, $id) : array_push($this->users, $id);
             }
+
+            /*foreach($response['items'] as $item) {
+                VkPost::updateOrCreate([
+                    'owner_id' => $item['owner_id'],
+                    'id' => $item['id']
+                ], $item);
+            };
+
+            $keys = array_reverse(array_column($response['items'], 'id'));
+            $posts = VkPost::where('owner_id', $item['owner_id'])
+                ->whereNotIn('id', $keys)
+                ->whereBetween('id', [current($keys), end($keys)])
+                ->get();
+            foreach ($posts as $post) {
+                $post->delete();
+            }*/
         }
     }
 }
